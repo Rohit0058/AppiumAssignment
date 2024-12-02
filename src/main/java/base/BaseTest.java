3c88b308df0f5b34f2d22e9bb85852a131471ca1
Rohit0058 <44227816+Rohit0058@users.noreply.github.com>
2024-12-02 17:49:49 +0530

    Adding Logging Functionality

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,15 +4,19 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import io.appium.java_client.ios.IOSDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.ConfigReader;
 
 import java.net.URL;
 import java.time.Duration;
 
 public class BaseTest {
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
     protected AppiumDriver driver;
 
     public void setup(String platform) throws Exception {
+        logger.info("Setting up the Appium driver and test environment");
         if (platform.equalsIgnoreCase("android")) {
             driver = new AppiumDriver(new URL(ConfigReader.get("serverUrl")), getAndroidCapabilities());
         } else if (platform.equalsIgnoreCase("ios")) {
@@ -44,6 +48,7 @@ public class BaseTest {
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+            logger.info("Appium session ended");
         }
     }
 }
